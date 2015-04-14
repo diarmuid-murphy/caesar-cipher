ad9777ceba85039daac04094e645d8238401eeb5
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-04-13 23:52:46 -0500

    batmam begins

---
+++ caesar_cipher.rb
@@ -0,0 +1,13 @@
+puts("Enter your text to encrypt: ")
+text = gets.chomp
+
+puts("What is your key?")
+key = gets.chomp
+
+
+text = text.split("")
+
+# remove this
+# text.each do |word|
+
+puts text.inspect
