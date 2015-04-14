aeafe7710b5467fc3deb7f3764ceae9596d80d1c
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-04-14 00:35:26 -0500

    more work needed

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -12,4 +12,15 @@ text.each do |char|
 	char_and_ord[char] = char.ord
 end
 
+result = ""
+
+# check if uppercase
+char_and_ord.each do |char, ord|
+	# if char_and_ord[char] 
+	
+	puts ord
+end
+
 puts char_and_ord.inspect
+puts text.inspect
+puts result
