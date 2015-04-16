da5207319659e623a97a5921395f61bb77e87ebf
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-04-15 22:27:13 -0500

    done :)

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,6 +4,8 @@ text = gets.chomp
 puts("What is your key?")
 key = gets.chomp
 
+key = key.to_i % 26
+
 puts ""
 puts "####### OUTPUT #######"
 puts ""
@@ -11,9 +13,23 @@ puts ""
 text = text.split("")
 
 text.each do |i|
-	print i
 	i = i.ord
+	
+	if (i >= 65) && (i <= 90)				# check if upppercase
+		i = i + key
+		if i > 90
+			i = i - 26
+		end
+		print i.chr
+	elsif (i >= 97) && (i <= 122)			# check if lowercase
+		i = i + key
+		if i > 122
+			i = i - 26
+		end
+		print i.chr
+	else									# deals with non-alpha characters, !. &* etc ect.
+		print i.chr
+	end
 end
 
-puts "\n"
-puts text.inspect
+puts "\n\n"
